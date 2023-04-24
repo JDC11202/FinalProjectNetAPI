09b26abf203387330f8a24073b1bffa86d93bfd1
JDC11202 <j.collier11202@gmail.com>
2023-04-24 18:04:56 -0400

    commit

--- Models/BasicUserInfo.cs
+++ Models/BasicUserInfo.cs
@@ -2,13 +2,10 @@ namespace FinalProjectNetAPI.Models;
 
 public class BasicUserInfo
 {
-<<<<<<< HEAD
     public int ID { get; set; }
     
-=======
->>>>>>> c4aa99f83a274d13cb618cc20c36a058720ef8bf
     public string Name { get; set; }
-
+    
     public DateOnly Birthdate { get; set; }
     
     public string Major { get; set; } 
