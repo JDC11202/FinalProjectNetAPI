c4aa99f83a274d13cb618cc20c36a058720ef8bf
JDC11202 <j.collier11202@gmail.com>
2023-04-23 21:01:32 -0400

    Current ver.

--- Models/BasicUserInfo.cs
+++ Models/BasicUserInfo.cs
@@ -2,11 +2,11 @@ namespace FinalProjectNetAPI.Models;
 
 public class BasicUserInfo
 {
-    public string? Name { get; set; }
+    public string Name { get; set; }
 
     public DateOnly Birthdate { get; set; }
     
-    public string? Major { get; set; } 
+    public string Major { get; set; } 
     
     public int year { get; set; }
 }
