bf54e8c37e33522d3c8938e4ed443035d45c8f8c
Dylan Mullarkey <DylanMullarkey10@gmail.com>
2023-04-23 21:26:20 -0400

    testing commit privledges

--- Models/BasicUserInfo.cs
+++ Models/BasicUserInfo.cs
@@ -2,11 +2,13 @@ namespace FinalProjectNetAPI.Models;
 
 public class BasicUserInfo
 {
-    public string? Name { get; set; }
+    public int ID { get; set; }
+    
+    public string Name { get; set; }
 
     public DateOnly Birthdate { get; set; }
     
-    public string? Major { get; set; } 
+    public string Major { get; set; } 
     
     public int year { get; set; }
 }
---
+++ Models/UserLocationInfo.cs
@@ -0,0 +1,13 @@
+namespace FinalProjectNetAPI.Models;
+
+public class BasicUserInfo
+{
+    public int ID { get; set; }
+    public string State { get; set; }
+
+    public string City { get; set; }
+    
+    public int AreaCode { get; set; } 
+    
+    public int ZipCode { get; set; }
+}
