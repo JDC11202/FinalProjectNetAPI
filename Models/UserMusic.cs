54b7919ce21e125aaea7441711bffcc1e266613a
rawlinsdrew3 <rawlinaj@mail.uc.edu>
2023-04-24 20:23:56 -0400

    updated music code

---
+++ Models/UserMusic.cs
@@ -0,0 +1,15 @@
+namespace FinalProjectAPI.Models;
+
+public class UserMusic
+{
+    public int Id { get; set; }
+
+    public int User_id { get; set; }
+
+    public int FavArtist { get; set; }
+
+    public int FavSong { get; set; }
+
+    public int FavGenre { get; set; }
+
+}
